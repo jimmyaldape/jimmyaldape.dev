e866f94621c39cd80beaae2d80e8def0c4487e24
Jimmy <jimmy.aldape@medsch.ucr.edu>
2020-08-07 01:29:01 -0700

    publishes first blog entry

--- source/blog.blade.php
+++ source/blog.blade.php
@@ -4,7 +4,9 @@
     <div class="min-w-screen min-h-screen flex items-center justify-center">
         <ul class="">
             @foreach ($posts as $post)
-                <li class="p-4"><a href="{{ $post->getPath() }}">{{ $post->title }}</a></li>
+                <li class="p-4">
+                    <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
+                </li>
             @endforeach
         </ul>
     </div>
