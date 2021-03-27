0a34f45337375db2d299ad5c08e03a38605640dd
Jimmy Aldape <jimmy@jimmyaldape.com>
2021-03-27 02:08:44 -0700

    updates posts

--- source/_posts/welcome-to-my-official-blog.md
+++
@@ -1,9 +0,0 @@
----
-extends: _layouts.post
-title: Welcome to my Blog
-description: Welcome to my blog. I am software engineer who loves to work with the TALL stack - Tailwind CSS, AlpineJS, Laravel, and Livewire.
-date: 2021-03-26
-section: content
----
-
-Hi! My name is Jimmy. Welcome to my blog. I am software engineer who loves to work with the TALL stack - Tailwind CSS, AlpineJS, Laravel, and Livewire. Follow along with me as I go through this continuous learning journey. I will try my best to document what I learn and what I'm interested in. If you like what you read, please follow me on twitter [@jimmyaldape](https://twitter.com/jimmyaldape). Thank you.
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,5 @@
 ---
-description: Jimmy Aldape is an experienced full-stack software engineer that specializes in the design, development, and architectural planning of software systems with a focus on web applications, database architecture and domain driven design who has a passion for designing beautiful and functional user experiences.
+description: Jimmy Aldape is an experienced software engineer that specializes in full-stack development, software system design with a focus on web applications, and who has a passion for designing beautiful and functional user experiences.
 ---
 @extends('_layouts.master')
 
@@ -7,7 +7,7 @@ description: Jimmy Aldape is an experienced full-stack software engineer that sp
     <div class="min-w-screen min-h-screen flex items-center justify-center">
         <div class="text-center ">
         <div class="text-3xl mb-4">Hello, my name is <span class="text-blue-500">Jimmy Aldape</span></div>
-        <p class="text-xl text-gray-500">I'm a full-stack software engineer who specializes in software system design and development with a focus on web applications who has a passion for designing beautiful and functional user experiences.</p>
+        <p class="text-xl text-gray-500">I'm a software engineer who specializes in full-stack development, software system design with a focus on web applications, and who has a passion for designing beautiful and functional user experiences.</p>
     </div>
     </div>
 @endsection
