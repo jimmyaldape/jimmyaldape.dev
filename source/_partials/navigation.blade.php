1b1d24c1233f9c159c826221b5c4b35080ccc021
Jimmy Aldape <jimmy@jimmyaldape.com>
2021-05-02 21:08:10 -0700

    removes resume

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -1,11 +1,6 @@
 <nav class="flex justify-between">
     <div class="hover:text-blue-300"><a href="{{'/blog'}}">blog</a></div>
     <div class="pl-4">
-        <a class="hover:text-blue-300" href="/assets/docs/jimmy_aldape_resume_current.pdf" target="_blank" title="View jimmy aldape's current resume">
-            resume
-        </a>
-    </div>
-    <div class="pl-4">
         <a class="hover:text-blue-300" href="https://twitter.com/jimmyaldape" target="_blank" title="Follow jimmy aldape on twitter">
             twitter
         </a>
