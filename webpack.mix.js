ea53e4fd5cafc513c82a4a159dd6d5e597c8a541
Jimmy Aldape <jimmy@jimmyaldape.com>
2021-05-02 21:07:42 -0700

    configures jit

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,13 +6,12 @@ mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
-    .postCss('source/_assets/sass/main.css', 'css', [
+    .postCss('source/_assets/css/main.css', 'css', [
         require('postcss-import'),
         require('tailwindcss'),
     ])
     .options({
         processCssUrls: false,
-        postCss: [ tailwindcss('./tailwind.config.js') ],
     })
     .version();
 
