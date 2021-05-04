62afd2a95fdd13ccc2477166698e81d76cde8325
Jimmy Aldape <jimmy@jimmyaldape.com>
2021-05-03 19:23:19 -0700

    updates formatting

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -1,13 +1,13 @@
-<nav class="links flex justify-between gap-2 lg:gap-8">
-    <div class="self-start">
+<nav class="links flex self-start justify-between gap-2 lg:gap-8">
+    <div>
         <a class="{{ $page->current('blog') }} uppercase" href="{{'/blog'}}" title="Jimmy's Blog">Blog</a>
     </div>
-    <div class="self-start">
+    <div>
         <a class="uppercase" href="https://twitter.com/jimmyaldape" target="_blank" title="Jimmy Aldape on Twitter">
             Twitter
         </a>
     </div>
-    <div class="self-start">
+    <div>
         <a class="uppercase" href="https://www.linkedin.com/in/jimmyaldape/" target="_blank" title="Jimmy Aldape on Linkedin">
             Linkedin
         </a>
