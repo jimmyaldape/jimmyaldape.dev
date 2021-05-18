dfb6ee0a63368844ab101b620c3255a6af7216a5
Jimmy Aldape <jimmy@jimmyaldape.com>
2021-05-18 14:32:50 -0700

    updates styling

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -1,14 +1,14 @@
-<nav class="links flex self-start justify-between gap-2 lg:gap-8">
+<nav class="links flex self-start justify-between gap-4 lg:gap-8">
     <div>
-        <a class="{{ $page->current('blog') }} uppercase" href="{{'/blog'}}" title="Jimmy's Blog">Blog</a>
+        <a class="{{ $page->current('blog') }}" href="{{'/blog'}}" title="Jimmy's Blog">Blog</a>
     </div>
     <div>
-        <a class="uppercase" href="https://twitter.com/jimmyaldape" target="_blank" title="Jimmy Aldape on Twitter">
+        <a class="" href="https://twitter.com/jimmyaldape" target="_blank" title="Jimmy Aldape on Twitter">
             Twitter
         </a>
     </div>
     <div>
-        <a class="uppercase" href="https://www.linkedin.com/in/jimmyaldape/" target="_blank" title="Jimmy Aldape on Linkedin">
+        <a class="" href="https://www.linkedin.com/in/jimmyaldape/" target="_blank" title="Jimmy Aldape on Linkedin">
             Linkedin
         </a>
     </div>
--- source/blog.blade.php
+++ source/blog.blade.php
@@ -11,25 +11,28 @@
     <div class="content  flex flex-col items-center justify-center">
         <h2 class="text-2xl uppercase text-gray-600 text-center mb-10">Blog</h2>
         @foreach ($posts as $post)
-                <section class="w-full">
-                    <div class="pb-20 w-4/5 lg:w-3/5 mx-auto">
-                        <div class="preview-thumbnail">
+            <section class="w-full">
+                <div class="mb-20 w-4/5 mx-auto">
+                    <div class="preview-thumbnail">
 
+                    </div>
+                    <div class="preview">
+                        <div>
+                            <a class="title text-2xl lg:text-3xl" href="{{ $post->getPath() }}">{{ $post->title }}</a>
                         </div>
-                        <div class="preview">
-                            <div>
-                                <a class="title text-2xl lg:text-3xl" href="{{ $post->getPath() }}">{{ $post->title }}</a>
-                            </div>
-                            <div>
-                                <span class="text-xs text-gray-400">{{ date('F j, Y', $post->date) }}</span>
-                            </div>
-                            <div class="snippet">
-                                {{ $post->snippet }}
-                            </div>
+                        <div>
+                            <span class="text-xs text-gray-400">{{ date('F j, Y', $post->date) }}</span>
+                        </div>
+                        <div class="snippet">
+                            {{ $post->snippet }}
+                        </div>
+                        <div>
+                            <p><a href="{{ $post->getPath() }}">Read More</a></p>
                         </div>
                     </div>
-                </section>
-            @endforeach
+                </div>
+            </section>
+        @endforeach
     </div>
 @endsection
 
